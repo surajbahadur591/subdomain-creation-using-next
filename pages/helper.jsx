f9cce9e13a5dbb3739768402c94e6c120b03b5b8
Suraj <surajbahadur58@gmail.com>
2023-05-13 09:46:38 +0530

    init

--- pages/helper.jsx
+++ pages/helper.jsx
@@ -1,5 +1,5 @@
 export const getSubDomain = (host) => {
     console.log('from helper function', host)
-    const arr = host.split(".").slice(0, host.includes("localhost") ? -1 : -3);
+    const arr = host.split(".").slice(0, host.includes("localhost") ? -1 : -2);
     return arr
 }
